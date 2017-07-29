47e2cac152bc42f17c4d7ca69edf46c5ad80e8c8
Luke <thelukexerxes@gmail.com>
2017-07-29 21:04:15 +1000

    Safety value per region complete

--- Durango.AxisPowers.Tech/ReadCSV.swift
+++ Durango.AxisPowers.Tech/ReadCSV.swift
@@ -26,8 +26,7 @@ func readCSVFile(fileName:String, fileType: String)-> [[String]]!{
 
 
 func readFile() {
-    //print(readCSVFile(fileName: "CrimeStats", fileType: "csv"))
-    print(CSVDataToPoints(data: readCSVFile(fileName: "CrimeStats", fileType: "csv")))
+    print(safetyRating(dictionary: CSVDataToPoints(data: readCSVFile(fileName: "CrimeStats", fileType: "csv"))))
 }
 
 
@@ -44,21 +43,46 @@ func CSVToArray(data: String) -> [[String]] {
     
 }
 
-func CSVDataToPoints(data: [[String]]) -> [String: [Int]] {
-    let pointsArray = [1, 2, 26, 81, 7, 53, 36, 47, 70]
+func CSVDataToPoints(data: [[String]]) -> [String: [Double]] {
+    print("")
+    let pointsArray = [1, 0.625, 0.5, 0.003125, 0.375, 0.375, 0.25, 0.5, 0.075]
     
-    var results = ["Region": [Int]()]
+    var results = [String:[Double]]()
     
     for i in 0 ... data.count-1 {
+        
         print(data[i][0])
-        var array = [Int]()
+        var array = [Double]()
         for j in 1...data[i].count-1 {
-            array.append(Int(data[i][j])! * pointsArray[j-1])
+            let amendedData = Int(data[i][j])! * 100
+            var amendedDataDouble = Double(amendedData)
+            amendedDataDouble = amendedDataDouble * pointsArray[j-1]
+            array.append(amendedDataDouble)
         }
+        
         results[data[i][0]] = array
+        
     }
     
     return results
     
+}
+
+func safetyRating(dictionary: [String: [Double]]) -> [String: Int] {
+    
+    var safetyValues = [String: Int]()
+    
+    for region in dictionary {
+        var regionTotal = 0.00
+        for points in region.value {
+            regionTotal += points
+        }
+        
+        safetyValues[region.key] = Int(round(regionTotal / 10000) * 2)
+        
+    }
+    
+    return safetyValues
+
 }
 
